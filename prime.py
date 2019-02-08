221c1e53bdf621edc6a83a6e1cc7e7cc45c33279
sarvath <47408223+sarvath@users.noreply.github.com>
2019-02-08 01:37:55 -0800

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,15 +1,15 @@
 while True:
 	try:
-		a=input()
+		b=input()
 		
-		a=int(a)
+		b=int(b)
 		break
 	except:
 		print("Invalid input");
 		break
-if a>1:
-	for x in range(2,a):
-		if a%x==0:
+if b>1:
+	for x in range(2,b):
+		if b%x==0:
 			print('no');
 			break
 		else:
