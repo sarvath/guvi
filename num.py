2a4cd743f6b29f556897f0adca33e8515ca17c86
sarvath <47408223+sarvath@users.noreply.github.com>
2019-02-07 21:54:58 -0800

    Update num.py

--- num.py
+++ num.py
@@ -1,12 +1,20 @@
 while True:
 	try:
-		a=int(input());
+		a,b,c=input().split()
+		a=int(a)
+		b=int(b)
+		c=int(c)
 		break
-	except:
-		print("Invalid input");
+	except: 
+		print("invalid input ")
 		break
-b=0
-for x in range(a):
-	b=b+x
-print(b+a
-)
+if (a>b):
+		if(a>c):
+				print(a);
+		elif c>a:
+				print(c);
+elif (b>a):
+	if (b>c):
+		print(b);
+	elif c>b:
+		print(c);
